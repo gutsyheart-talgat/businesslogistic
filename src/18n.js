5f35854ed221d27a0c26cfafa3d6b05b1b93e616
unknown <Talgat>
2024-11-01 12:25:56 +0600

    translator

--- src/18n.js
+++ src/18n.js
@@ -2,9 +2,21 @@ import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import translationEN from "../src/translationEN.json";
+import translationRU from "../src/translationRU.json";
+
+ const resources = {
+   en: {
+     translation: translationEN
+   },
+   ru: {
+     translation: translationRU
+   }
+ };
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'ru',
+    resources,
+    lng: "ru",
     debug: true,
     detection:{
         order: ['queryString', 'cookie'],
